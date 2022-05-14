aa3b38203d6d84d154bf2ec104dcc170cc1a9ce4
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-14 13:34:40 -0500

    uploading project

---
+++ pages/practica.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Practica = () =>{
+    return(
+        <div>
+            <p>Practica 2</p>
+            <Link href="/">Inicio</Link>
+        </div>
+    )
+}
+
+
+export default Practica
