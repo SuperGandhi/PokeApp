5012165ab0122516a9ccfac5ca3bca50da424e28
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-14 13:55:44 -0500

    call to api and integrating component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
-
-export default function Home() {
+const Pokemon = ({pokemon}) =>{
   return (
-      <p>Pokemons</p>
+    <li>{pokemon.name}</li>
   )
+}
+
+export default function Pokemons({pokemons}) {
+
+  return (
+    <div>
+      <p>Pokemons</p>
+      <ul>
+        {pokemons.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name}/>)}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps =  async () =>{
+  const response = await  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  const data = await response.json()
+  return {
+    props: {pokemons: data.results}
+  }
 }
