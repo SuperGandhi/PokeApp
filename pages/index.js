39703c63cdbf76b21c181b4b5fafa7c1eccc6e74
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-15 00:00:00 -0500

    adding css, document, images

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,27 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 
-const Pokemon = ({pokemon}) =>{
-  const id= pokemon.url.split('/').filter(x => x).pop()
+const Pokemon = ({ pokemon }) => {
+  const id = pokemon.url.split('/').filter(x => x).pop()
   return (
-    <li><Link href={`pokemones/${id}`}>{pokemon.name}</Link></li>
+    <li><Link href={`/pokemones/${id}`}>{pokemon.name}</Link></li>
   )
 }
-
-export default function Pokemons({pokemons}) {
-
+export default function Pokemones({ pokemones }) {
   return (
     <div>
-      <p>Pokemons</p>
+      <p>Mi App de Pokemones</p>
       <ul>
-        {pokemons.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name}/>)}
+        {pokemones.map(pokemon => <Pokemon pokemon={pokemon} key={pokemon.name} />)}
       </ul>
     </div>
   )
 }
 
-// properties statics
-
-export const getStaticProps =  async () =>{
-  const response = await  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+export const getStaticProps = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
   const data = await response.json()
+
   return {
-    props: {pokemons: data.results}
+    props: { pokemones: data.results }
   }
 }
